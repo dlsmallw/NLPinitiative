9714cac714ace3f8925524d5192e69ae5ddeff67
dlsmallw <dlsmallw@asu.edu>
2025-01-27 11:12:15 +0900

    Task-193 Initialized the NLP model codebase file structure

---
+++ nlpinitiative/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Codebase for training, evaluating and deploying an NLP model used to detect misinformation and/or discriminatory language targetting marginallized individuals or communities.',
+    author='ASU Capstone Group 8 / J Initiative',
+    license='MIT',
+)
